1d7c931e3a6ea75a9857a23fcf6d99c7c8cf4ce9
GA <ga2024@Alis-MacBook-Pro-3.local>
2025-04-09 20:49:00 +0300

    Adding the Transaction API and Connected with Dashboard

--- ExpenseTracker/src/pages/Dashboard.jsx
+++ ExpenseTracker/src/pages/Dashboard.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Pie } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { useState, useEffect, useContext } from 'react'
+import { getAllTransaction } from '../service/transactionService'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 const Dashboard = () => {
+  const [transactions, setTransactions] = useState([])
+
+  async function getTransactions() {
+    try {
+      const fetchedTransactions = await getAllTransaction()
+      setTransactions(fetchedTransactions)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getTransactions()
+  },[])
+
   const pieData1 = {
     labels: ['Completed', 'Pending', 'In Progress'],
     datasets: [
@@ -20,7 +37,7 @@ const Dashboard = () => {
     labels: ['Success', 'Failure'],
     datasets: [
       {
-        data: [85, 15],
+        data: [85, 30],
         backgroundColor: ['#4CAF50', '#F44336'],
         hoverOffset: 4,
       },
@@ -39,8 +56,8 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
-      <h5 className="greeting">Estimated Budget : $100,000</h5>
+    <div className='main-content'>
+      <h5 className="dasboard-title">Estimated Budget : $100,000</h5>
 
       <div className="chart-container">
         <div className="chart-card">
@@ -58,6 +75,26 @@ const Dashboard = () => {
           <Pie data={pieData3} options={{responsive: true, plugins: {legend: {position: 'bottom'}, tooltip: {enabled: true}}}}/>
         </div>
       </div>
+      <h5 className='dasboard-title'>Transactions</h5>
+      {
+        transactions.map((transaction) => {
+          return (
+            <div className="full-width-card" key={transaction._id}>
+              <div>
+                <div className="card-header">
+                  <h3 className="transaction-title">{transaction.category_id.name}</h3>
+                  <h3 className="transaction-title">Spend: $253,872</h3>
+                </div>
+                <h2 className='transaction-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia, lorem non feugiat tristique, elit orci aliquam risus, vel venenatis ligula tortor a mauris. Proin ac urna ut nulla tincidunt laoreet. Integer gravida, erat sit amet gravida condimentum, velit magna fermentum est, at sodales elit sapien a libero. Curabitur lobortis ipsum a felis vestibulum, ac auctor lorem consectetur. Nulla facilisi. Suspendisse potenti. Ut eu orci ut orci gravida pretium non in turpis. Mauris ac felis sit amet risus convallis tincidunt.</h2>
+                <div className="card-header">
+                  <h2 className='transaction-sub-info'>Done through: Online Payment</h2>
+                  <h2 className='transaction-sub-info'>Created at: 20-10-2025</h2>
+                </div>
+              </div>
+            </div>
+          )
+  })
+}
     </div>
   )
 }
---
+++ ExpenseTracker/src/service/transactionService.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/transaction`;
+
+const api = axios.create({
+    baseURL: BASE_URL 
+});
+  
+//   api.interceptors.request.use(config => {
+//       const token = localStorage.getItem('token');
+//       if (token) {
+//           config.headers.Authorization = `Bearer ${token}`;
+//       }
+//       return config;
+//   });
+  
+const getAllTransaction = async () => {
+    try {
+        const res = await api.get('/1/10'); 
+        console.log('res', res.data.data);
+        return res.data.data;
+    } catch (error) {
+        console.log('Error fetching data:', error);
+    }
+};
+
+
+    
+//   const getOneHoot = async (id) => {
+//     try {
+//         const res = await api.get(`/${id}`); 
+//         return res.data;
+//     } catch (error) {
+//         console.log('Error fetching data:', error);
+//     }
+// };
+
+// async function deleteHoot(id){
+//     try{
+//         const res = await api.delete(`/${id}`)
+//         return res.data
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+
+// async function createHoot(formData){
+//     try{
+//         const res = await api.post(`/`,formData)
+//         return res.data
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+  
+
+export { 
+    getAllTransaction,
+    // getOneHoot,
+    // deleteHoot,
+    // createHoot
+};
